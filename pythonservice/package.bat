47966366237d6e6939d706aa5b74df91b6c3c220
shensong <shensong@lixiang.com>
2019-12-02 16:45:37 +0800

    delete pythonservice
    Signed-off-by: shensong <shensong@lixiang.com>

--- pythonservice/package.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo remove old file
-rd/s/q .\dist
-pyinstaller manage.spec
-echo copy requirement file
-xcopy .\brainstem .\dist\python_service\brainstem\ /e
-xcopy .\python_service.bat .\dist\python_service
-echo package complete
-pause
--- pythonservice/python_service.bat
+++
@@ -1,1 +0,0 @@
-python_service.exe runserver
