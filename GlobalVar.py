d9197776a260e2dfa06a46f8e4332318379ca029
shensong <shensong@lixiang.com>
2019-12-16 17:19:31 +0800

    update...
    Signed-off-by: shensong <shensong@lixiang.com>

--- GlobalVar.py
+++ GlobalVar.py
@@ -120,6 +120,7 @@ class IconPath:
     Icon_main_tab_widget_open_file             = 'config/Icon/main_tab_widget_icon/open_file.png'
     Icon_main_tab_widget_edit_text             = 'config/Icon/main_tab_widget_icon/edit_text.png'
     Icon_main_tab_widget_save_text             = 'config/Icon/main_tab_widget_icon/save_text.png'
+    Icon_main_tab_widget_close_tab             = 'config/Icon/main_tab_widget_icon/close_tab.png'
 
 
 class RobotArmAction:
